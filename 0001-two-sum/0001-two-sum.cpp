358b26897217931a6faaa0d6681897436c9b7e6b
Sahil Chand <chandsahil2004@gmail.com>
2026-07-18 23:51:34 +0530

    Time: 3 ms (66.56%), Space: 14.8 MB (55.65%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,16 +1,35 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        //Create HashMap
+
+ /*for each element
+
+
+    complement = target - nums[i]
+
+    if complement exists in map
+        return indices
+
+    otherwise
+        put current number in map
+
+        i will minus the index with the target to check if the rrequired number is there if there print its index..
         
-        for(int i=0;i< nums.size();i++){
-            int one= i;
-            for(int j=i+1;j<nums.size();j++){
-                if(nums[one]+nums[j]==target){
-                    return {one,j};
-                }
-            }
+    }*/
+    unordered_map<int,int>mp;
+    for ( int i =0 ;i<nums.size();i++){
+        int left= target - nums[i];
+
+        if(mp.find(left)!=mp.end()){ // left ko find kr last tk
+            return {mp[left],i};
         }
-        
-        return {};
+        //nahi mila to
+        mp[nums[i]]=i;
     }
+    
+ return {};
+    };
+
 };
+
