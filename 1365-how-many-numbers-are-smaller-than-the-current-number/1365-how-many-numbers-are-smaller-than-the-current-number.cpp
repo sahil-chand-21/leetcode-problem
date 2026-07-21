7912da01b3bbf42d9498b9e430da8b0a115afe3e
Sahil Chand <chandsahil2004@gmail.com>
2026-07-21 22:13:47 +0530

    Time: 4 ms (60.54%), Space: 14.7 MB (15.11%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -2,19 +2,52 @@ class Solution {
 public:
     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
         // brute force approach  time complexity is 0(n^2)
+        // int n= nums.size();
+        // vector<int>ans(n);
+        // for(int i=0;i<n;i++){
+        //     int count =0;
+        //     for(int j=0;j<n;j++){
+        //         if(nums[j]<nums[i]){
+        //             count++;
+        //         }
+
+        //     }
+        //     ans[i] =count;
+        // }
+        // return ans;
+        
+        // the optimal approach is SORT AND HASHmap
         int n= nums.size();
-        vector<int>ans(n);
-        for(int i=0;i<n;i++){
-            int count =0;
-            for(int j=0;j<n;j++){
-                if(nums[j]<nums[i]){
-                    count++;
+
+        
+        vector<int>cp=nums;
+       
+        sort(cp.begin(),cp.end());
+        // cp=1,1,2,2,3,8
+        
+        unordered_map<int,int>map;
+        for(int i =0 ;i<n;i++){
+            if( map.find(cp[i]) == map.end()){
+                map[cp[i]]=i;
+            }
+        }
+            // {
+                // 1:0
+                // 2:2
+                // 3:4
+                // 8:5
+                // }
+                // now we have make vector to return only index
+                // 0,0,2,2,4,5
+                //iske aage ni aya
+                vector<int>ans;
+                for(int i=0;i<n;i++){
+                    ans.push_back(map[nums[i]]);
+
+
                 }
 
-            }
-            ans[i] =count;
-        }
-        return ans;
         
+    return ans;
     }
 };
