30d180ae8b1699503818aa517bd430c875a3164f
Sahil Chand <chandsahil2004@gmail.com>
2026-08-07 09:29:48 +0530

    Time: 3 ms (20.12%), Space: 21.8 MB (71.45%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
-    int missingNumber(vector<int>& nums) {
-        int n = nums.size()+1;
-        unordered_set<int>st(nums.begin(),nums.end());
-        int ans =0;
-        for(int i =0 ; i<n ;i++){
-            if(st.find(i)==st.end()){
-                ans =i; 
-                break;
-            }
-
+    int missingNumber(vector<int>& nums) { // cane be done by sorting also
+    
+         int x=nums.size();
+        int ans=x;
+        for(int i=0;i<x;i++){
+            ans= ans^i^nums[i]; // two xor  i ^elem^ //smjhoo
         }
         return ans;
+        
     }
+        
 };
