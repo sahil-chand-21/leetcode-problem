db39b050ab4297bba561dffb0cfd992d2a7c98a2
Sahil Chand <chandsahil2004@gmail.com>
2026-09-08 20:47:56 +0530

    Time: 0 ms (100%), Space: 8.6 MB (51.38%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int countCommas(int n) {
-        return max(n-999,0);
+        int count =0;
+        for(int i=1000;i<=n;i++){
+            count++;
+        }
+        return count;
     }
 };
