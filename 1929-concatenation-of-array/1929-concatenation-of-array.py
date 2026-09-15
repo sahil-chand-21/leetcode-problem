fb8df3dfe34b129ca61295ba97fe9da9f1f46419
Sahil Chand <chandsahil2004@gmail.com>
2026-09-15 16:59:58 +0530

    Time: 0 ms (100%), Space: 12.7 MB (35.47%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        ans=nums*2
+        return ans
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        
