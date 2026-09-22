2d1307f8e3446684c25059e53fe15ee74b5b993b
Sahil Chand <chandsahil2004@gmail.com>
2026-09-22 22:02:41 +0530

    Time: 13 ms (31.68%), Space: 7.9 MB (37.65%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -4,8 +4,6 @@ public:
         if( n<=1){
             return n;
         }
-        int one =fib(n-1);
-        int two=fib(n-2);
-        return one+two;
+        return fib(n-1)+fib(n-2);
     }
 };
